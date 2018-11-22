a2109a7b8f3b1ba4c497ec9734ff09c66c74d3b2
fallen <rogerk08@126.com>
2018-11-22 23:21:40 +0800

    *

--- wechat/Traits/WechatAdminOauthTrait.php
+++ wechat/Traits/WechatAdminOauthTrait.php
@@ -6,11 +6,13 @@
  */
 namespace Wechat\Traits;
 
+use Admin\Services\User\Integration\UserRegisterIntegration;
 use Admin\Services\User\Processor\UserProcessor;
 use Carbon\Carbon;
 use Common\Models\User\User;
 use Overtrue\Socialite\User as SocialiteUser;
 use Wechat\Config\WechatConfig;
+use Wechat\Tool\WechatTool;
 
 trait WechatAdminOauthTrait
 {
@@ -60,25 +62,25 @@ trait WechatAdminOauthTrait
         $user = User::where(['openid' => $wechatUser->id])->first();
         if (empty($user)) {
             // 测试
-            $data = [
-                'nick_name'         => array_get($wechatUser, 'nickname'),
-                'openid'            => array_get($wechatUser, 'id'),
-                'face'              => array_get($wechatUser, 'avatar'),
-                'last_login_at'     => date('Y-m-d H:i:s'),
-            ];
-            list($status, $user) = (new UserProcessor())->insert($data);
-            if ($status) {
-                return $user->id;
-            }
-            return 0;
-            // 正式
-//            $wechatTool = new WechatTool();
-//            $register = new UserRegisterIntegration($wechatTool->getUserInfo($wechatUser->id));
-//            list($status, $errMsg, $socialiteUser, $userId) = $register->process();
+//            $data = [
+//                'nick_name'         => array_get($wechatUser, 'nickname'),
+//                'openid'            => array_get($wechatUser, 'id'),
+//                'face'              => array_get($wechatUser, 'avatar'),
+//                'last_login_at'     => date('Y-m-d H:i:s'),
+//            ];
+//            list($status, $user) = (new UserProcessor())->insert($data);
 //            if ($status) {
-//                return $userId;
+//                return $user->id;
 //            }
 //            return 0;
+            // 正式
+            $wechatTool = new WechatTool();
+            $register = new UserRegisterIntegration($wechatTool->getUserInfo($wechatUser->id));
+            list($status, $errMsg, $socialiteUser, $userId) = $register->process();
+            if ($status) {
+                return $userId;
+            }
+            return 0;
         } else {
             $user->last_login_at = Carbon::now();
             $user->save();
