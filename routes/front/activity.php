aa5d8b83d87536bf28ca28aeefa603d2b3c8ca29
jiaolongtian <haiyan.xu.vip@gmail.com>
2018-11-26 16:47:06 +0800

    *

--- routes/front/activity.php
+++ routes/front/activity.php
@@ -1,12 +1,12 @@
 <?php
 // 投票操作
-Route::group(function () {
+Route::middleware(['web'])->group(function () {
     Route::match(['get', 'post'], '/front/activity/vote', [
         'uses' => '\App\Http\Front\Controllers\Activity\VoteController@info'
-    ])->middleware(['web','front.login.auth'])->name('front.activity.vote');
+    ])->middleware('front.login.auth')->name('front.activity.vote');
     Route::match(['get', 'post'], '/front/activity/feedback', [
         'uses' => '\App\Http\Front\Controllers\Activity\VoteController@feedback'
-    ])->middleware(['web'])->name('front.activity.feedback');
+    ])->name('front.activity.feedback');
 });
 // 活动操作
 Route::middleware(['web'])->group(function () {
@@ -15,11 +15,11 @@ Route::middleware(['web'])->group(function () {
     ])->name('front.activity.like');
 });
 // 网络投票
-Route::group(function () {
+Route::middleware(['web'])->group(function () {
     Route::match(['get', 'post'], '/front/activity/polls', [
         'uses' => '\App\Http\Front\Controllers\Activity\PollController@index'
-    ])->middleware(['web'])->name('front.polls');
+    ])->name('front.polls');
     Route::get('/front/activity/poll/{code}', [
         'uses' => '\App\Http\Front\Controllers\Activity\PollController@info'
-    ])->middleware(['web','front.login.auth'])->name('front.poll.info');
+    ])->middleware('front.login.auth')->name('front.poll.info');
 });
--- routes/front/interact.php
+++ routes/front/interact.php
@@ -1,13 +1,13 @@
 <?php
 // 师生互动
-Route::group(function () {
+Route::middleware(['web'])->group(function () {
     Route::get('/front/interact/admonitions', [
         'uses' => '\App\Http\Front\Controllers\Interact\AdmonitionController@index'
-    ])->middleware(['web','front.login.auth'])->name('front.admonitions');
+    ])->middleware('front.login.auth')->name('front.admonitions');
     Route::match(['get', 'post'], '/front/interact/admonition/consult', [
         'uses' => '\App\Http\Front\Controllers\Interact\AdmonitionController@consult'
-    ])->middleware(['web','front.login.auth'])->name('front.admonition.consult');
+    ])->middleware('front.login.auth')->name('front.admonition.consult');
     Route::get('/front/interact/admonition/feedback', [
         'uses' => '\App\Http\Front\Controllers\Interact\AdmonitionController@feedback'
-    ])->middleware(['web'])->name('front.admonition.feedback');
+    ])->name('front.admonition.feedback');
 });
